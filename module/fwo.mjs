bb6d7542ffacaf6d4576d520b6bee97a6dd1e2ef
Ishkander <95763005+Ishkander56@users.noreply.github.com>
2024-05-06 13:50:41 -0500

    I made this change like two years ago in private

--- module/fwo.mjs
+++ module/fwo.mjs
@@ -225,7 +225,7 @@ async function createItemMacro(data, slot) {
 
   // Create the macro command
   const command = `game.fwo.rollItemMacro("${item.name}");`;
-  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
   if (!macro) {
     macro = await Macro.create({
       name: item.name,
